209cd0c8eab12ad0230160d9f32ac420b2195aa5
YuriHandalan <cocbdgsale1@gmail.com>
2024-03-14 14:07:22 +0700

    12

--- c12.js
+++ c12.js
@@ -1,47 +1,56 @@
 if (process.argv[2]) {
-    const fs = require("fs")
-    const data = fs.readFileSync(`${process.argv[2]}`, "utf-8")
-    const quest = JSON.parse(data)
+
+    const { readFileSync } = require('node:fs')
+    const data = readFileSync(`${process.argv[2]}`, "utf-8")
+    const json = JSON.parse(data)
     const readline = require("node:readline")
-    
-    const readl = readline.createInterface({
+
+    const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
-        prompt: "Jawaban : "
-    })
+        prompt: 'Tebakan : ',
+    });
 
     console.log("Selamat datang di permainan Tebak-tebakan. Kamu akan diberikan pertanyaan dari file ini 'data.json'. Untuk bermain, jawablah dengan jawaban sesuai",
         "Gunakan 'skip' untuk menangguhkan pertanyaannya , dan di akhir pertanyaan akan di tanya lagi.")
 
-    let counter = 0
-    let salah = 0
+    let repeat = 0
+    let wrong = 0
 
-    console.log("Pertanyaan : " + quest[counter].definition)
+    console.log("Pertanyaan : " + json[repeat].definition)
 
-    readl.prompt()
-    readl.on("line", (jawaban) => {
-        if (jawaban.toString().toLowerCase() == quest[counter].term) {
-            console.log("Anda Beruntung !\n")
-            counter++
-            salah = 0
-            if (counter == quest.length) {
-                console.log("Anda Berhasil \n")
-                readl.close()
-            }
-            console.log("Pertanyaan : " + quest[counter].definition)
-        } else if (jawaban.toString().toLowerCase() == "skip") {
-            quest.push(quest[counter])
-            counter++
-            salah = 0
-            console.log("Pertanyaan : " + quest[counter].definition)
+    rl.prompt();
+    rl.on("line", (jawaban) => {
+        if (jawaban.toLowerCase() == "skip") {
+            json.push(json[repeat])
+            repeat++
+            // console.log(json)
+            wrong = 0
+            console.log("Pertanyaan : " + json[repeat].definition)
         } else {
-            salah++
-            console.log(`Anda Kurang Beruntung , Anda salah ${salah} silahkan coba lagi`)
+
+            if (jawaban.toString().toLowerCase() == json[repeat].term) {
+                console.log("Selamat Anda Benar!")
+                repeat++
+                wrong = 0
+                if (repeat == json.length) {
+                    console.log("Hore , Anda Menang!")
+                    rl.close()
+                }
+
+                console.log("Pertanyaan : " + json[repeat].definition)
+            } else {
+                wrong++
+                console.log(`Anda Kurang Beruntung!, Anda salah ${wrong} silahkan coba lagi`)
+            }
         }
-        readl.prompt()
-    }).on("close", () => {
-        process.exit(0)
+        rl.prompt()
     })
+
+        .on("close", () => {
+            process.exit(1)
+        })
+
 } else {
     console.log("Tolong serta kan nama file sebagai inputan soal-soalnya , Misalnya 'node c12.js  data.json' . \n")
     process.exit()
